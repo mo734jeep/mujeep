dae4bdcfe77f8eed0792bb45d72cfe94a3c4f403
mujeep <mujeep@gmail.com>
2025-09-19 00:34:53 +0300

    adding splash file to project

---
+++ splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:myproject/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    });
+  }
+
