56e3bb9b913446449ed293f08db7d863d61c5059
mujeep <mujeep@gmail.com>
2025-09-20 00:17:52 +0300

    updating splash_screen_file v4

--- generate_password_page.dart
+++ generate_password_page.dart
@@ -8,3 +8,32 @@ class GeneratePasswordPage extends StatefulWidget {
   State<GeneratePasswordPage> createState() => _GeneratePasswordPageState();
 }
 
+class _GeneratePasswordPageState extends State<GeneratePasswordPage> {
+  int passwordLength = 12;
+  bool includeNumbers = true;
+  bool includeSymbols = true;
+  bool includeUppercase = true;
+
+  String generatedPassword = "";
+
+  void generatePassword() {
+    const String lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const String numbers = "0123456789";
+    const String symbols = "!@#\$%^&*()-_=+<>?";
+
+    String chars = lowercase;
+    if (includeUppercase) chars += uppercase;
+    if (includeNumbers) chars += numbers;
+    if (includeSymbols) chars += symbols;
+
+    Random random = Random.secure();
+    String password = List.generate(
+      passwordLength,
+          (index) => chars[random.nextInt(chars.length)],
+    ).join();
+
+    setState(() {
+      generatedPassword = password;
+    });
+  }
